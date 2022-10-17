f64d278fe0c4adb7e3e33b79a8282a9006bbaa66
Dai <feng.dai@intel.com>
2022-10-17 13:41:16 +0800

    Optimize tests using r(runtime) parameter, Add all-in-one ops.html entry

---
+++ webnn/ops.https.any.worker.js
@@ -0,0 +1,10 @@
+self.GLOBAL = {
+  isWindow: function() { return false; },
+  isWorker: function() { return true; },
+  isShadowRealm: function() { return false; },
+};
+importScripts("../resources/testharness.js");
+self.META_TITLE = "test WebNN API operations";
+importScripts("./resources/utils.js")
+importScripts("./ops.https.any.js");
+done();
