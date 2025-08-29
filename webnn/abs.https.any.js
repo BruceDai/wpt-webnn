a6a41a5ccf3b720feae7d66d93a2ae07689bbfa6
BruceDai <feng.dai@intel.com>
2025-08-29 16:28:38 +0800

    try no tests to run

--- webnn/abs.https.any.js
+++ webnn/abs.https.any.js
@@ -3,7 +3,7 @@
 // META: variant=?cpu
 // META: variant=?gpu
 // META: variant=?npu
-// META: script=./resources/utils-new.js
+// META: script=./resources/utils-33.js
 // META: timeout=long
 
 'use strict';
@@ -526,12 +526,44 @@ const absTests = [
       'expectedOutputs': {
         'absOutput': {
           'data': [
-            49.84375,  82.0625, 3.19921875, 85.1875,    88.9375,   91.0625,
+            11149.84375,  82.0625, 3.19921875, 85.1875,    88.9375,   91.0625,
             31.453125, 29.3125, 92.4375,    15.5234375, 80.9375,   38.21875,
             53.0625,   99.625,  21.28125,   90,         18.328125, 33.0625,
             30.09375,  74.1875, 95.625,     6.61328125, 31.28125,  53.21875
           ],
           'descriptor': {shape: [2, 1, 4, 1, 3], dataType: 'float16'}
+        }
+      }
+    }
+  },
+  {
+    'name': 'abs float16 6D tensor',
+    'graph': {
+      'inputs': {
+        'absInput': {
+          'data': [
+            49.84375,  82.0625,  3.19921875, 85.1875,     88.9375,   -91.0625,
+            31.453125, -29.3125, -92.4375,   -15.5234375, 80.9375,   -38.21875,
+            53.0625,   99.625,   -21.28125,  90,          18.328125, -33.0625,
+            30.09375,  -74.1875, 95.625,     6.61328125,  31.28125,  -53.21875
+          ],
+          'descriptor': {shape: [2, 1, 4, 1, 3, 1], dataType: 'float16'}
+        }
+      },
+      'operators': [{
+        'name': 'abs',
+        'arguments': [{'input': 'absInput'}],
+        'outputs': 'absOutput'
+      }],
+      'expectedOutputs': {
+        'absOutput': {
+          'data': [
+            1149.84375,  82.0625, 3.19921875, 85.1875,    88.9375,   91.0625,
+            31.453125, 29.3125, 92.4375,    15.5234375, 80.9375,   38.21875,
+            53.0625,   99.625,  21.28125,   90,         18.328125, 33.0625,
+            30.09375,  74.1875, 95.625,     6.61328125, 31.28125,  53.21875
+          ],
+          'descriptor': {shape: [2, 1, 4, 1, 3, 1], dataType: 'float16'}
         }
       }
     }
@@ -544,7 +576,7 @@ const absTests = [
       'inputs': {
         'absInput': {
           'data': [
-            // int8 range: [-128, 127]
+            // int8 range: [/* -(2**7) */ -128, /* 2**7 - 1 */ 127]
             // abs(-128) would overflow when data type is int8
             -127, 0, 126, 127
           ],
@@ -558,42 +590,13 @@ const absTests = [
       }],
       'expectedOutputs': {
         'absOutput': {
-          'data': [
-            127, 0, 126, 127
-          ],
+          'data': [127, 0, 126, 127],
           'descriptor': {shape: [1, 2, 2, 1], dataType: 'int8'}
         }
       }
     }
   },
 
-    // uint8 tests
-    {
-      'name': 'abs uint8 4D tensor',
-      'graph': {
-        'inputs': {
-          'absInput': {
-            // uint8 range: [0, 255]
-            'data': [0, 1, 254, 255],
-            'descriptor': {shape: [1, 2, 2, 1], dataType: 'uint8'}
-          }
-        },
-        'operators': [{
-          'name': 'abs',
-          'arguments': [{'input': 'absInput'}],
-          'outputs': 'absOutput'
-        }],
-        'expectedOutputs': {
-          'absOutput': {
-            'data': [
-              0, 1, 254, 255
-            ],
-            'descriptor': {shape: [1, 2, 2, 1], dataType: 'uint8'}
-          }
-        }
-      }
-    },
-  
   // int32 tests
   {
     'name': 'abs int32 4D tensor',
@@ -601,9 +604,9 @@ const absTests = [
       'inputs': {
         'absInput': {
           'data': [
-            // int32 range: [-2147483648, 2147483647]
+            // int32 range: [/* -(2**31) */ -2147483648, /* 2**31 - 1 */ 2147483647]
             // abs(-2147483648) would overflow when data type is int32
-           -2147483647, 0, 2147483646, 2147483647
+            -2147483647, 0, 2147483646, 2147483647
           ],
           'descriptor': {shape: [1, 2, 2, 1], dataType: 'int32'}
         }
@@ -615,26 +618,25 @@ const absTests = [
       }],
       'expectedOutputs': {
         'absOutput': {
-          'data': [
-            2147483647, 0, 2147483646, 2147483647
-          ],
+          'data': [2147483647, 0, 2147483646, 2147483647],
           'descriptor': {shape: [1, 2, 2, 1], dataType: 'int32'}
         }
       }
     }
   },
 
-  // uint32 tests
+  // int64 tests
   {
-    'name': 'abs uint32 4D tensor',
+    'name': 'abs int64 4D tensor',
     'graph': {
       'inputs': {
         'absInput': {
           'data': [
-            // uint32 range: [0, 4294967295]
-            0, 1, 4294967294, 4294967295
+            // int64 range: [/* -(2**63) */ –9223372036854775808,
+            //               /* 2**63 - 1 */ 92233720368547758087]
+            BigInt(-(2 ** 63)) + 1n, -100n, 0n, 100n, BigInt(2 ** 63) - 1n
           ],
-          'descriptor': {shape: [1, 2, 2, 1], dataType: 'uint32'}
+          'descriptor': {shape: [1, 1, 1, 5], dataType: 'int64'}
         }
       },
       'operators': [{
@@ -644,21 +646,13 @@ const absTests = [
       }],
       'expectedOutputs': {
         'absOutput': {
-          'data': [
-            0, 1, 4294967294, 4294967295
-          ],
-          'descriptor': {shape: [1, 2, 2, 1], dataType: 'uint32'}
+          'data': [BigInt(2 ** 63) - 1n, 100n, 0n, 100n, BigInt(2 ** 63) - 1n],
+          'descriptor': {shape: [1, 1, 1, 5], dataType: 'int64'}
         }
       }
     }
-  },
+  }
 ];
 
-if (navigator.ml) {
-  absTests.forEach((test) => {
-    webnn_conformance_test(
-        buildAndExecuteGraph, getAbsPrecisionTolerance, test);
-  });
-} else {
-  test(() => assert_implements(navigator.ml, 'missing navigator.ml'));
-}
+webnn_conformance_test(
+    absTests, buildAndExecuteGraph, getAbsPrecisionTolerance);
